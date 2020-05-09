1eb66fc3c335ae68e2abf7bd8e7236ae7b494a98
Brian <brianzzl@hotmail.com>
2020-05-09 17:27:23 -0400

    deployed token and transfer all tokens  to exchange contract

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,15 @@
+const PikaSwap = artifacts.require("PikaSwap");
+const Token = artifacts.require("Token");
+
+module.exports = async function(deployer) {
+  //Deploy Token and PikaSwap
+
+  await deployer.deploy(Token);
+  const token = await Token.deployed();
+
+  await deployer.deploy(PikaSwap);
+  const pikaSwap = await PikaSwap.deployed();
+
+  //Transfer all token to PikaSwap
+  await token.transfer(pikaSwap.address, "1000000000000000000000000");
+};
---
+++ src/contracts/PikaSwap.sol
@@ -0,0 +1,6 @@
+pragma solidity >=0.5.0;
+
+
+contract PikaSwap {
+    string public name = " Pikachill Pokecoin Exchange";
+}
