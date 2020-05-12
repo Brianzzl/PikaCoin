495bc599a95f6fa80c5fe38eeb177e52527f8d27
Brian <brianzzl@hotmail.com>
2020-05-12 16:03:27 -0400

    load initial data from blockchain and nav component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,118 @@
 import React, { Component } from "react";
-
-import "./App.css";
+import Web3 from "web3";
+import Token from "../abis/Token.json";
+import PikaSwap from "../abis/PikaSwap.json";
+import Navbar from "./Navbar";
+import Main from "./Main";
 
 class App extends Component {
+  async componentDidMount() {
+    await this.loadWeb3();
+    // console.log(window.web3);
+    await this.loadBlockChainData();
+  }
+
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+    } else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+    } else {
+      window.alert(
+        "Non-Ethereum browser detected. You should consider trying MetaMask!"
+      );
+    }
+    // console.log(window.web3);
+  }
+
+  async loadBlockChainData() {
+    const web3 = window.web3;
+
+    const accounts = await web3.eth.getAccounts();
+    console.log(accounts);
+    this.setState({ account: accounts[0] });
+    console.log(this.state.account);
+
+    const ethBalance = await web3.eth.getBalance(this.state.account);
+    this.setState({ ethBalance });
+    console.log(this.state.ethBalance);
+
+    //load token
+    const abi = Token.abi;
+    const networkId = await web3.eth.net.getId();
+    const tokenData = Token.networks[networkId];
+    if (tokenData) {
+      const address = tokenData.address;
+      const token = new web3.eth.Contract(abi, address);
+      this.setState({ token });
+
+      let tokenBalance = await token.methods
+        .balanceOf(this.state.account)
+        .call();
+      console.log(tokenBalance.toString());
+      this.setState({
+        tokenBalance: tokenBalance.toString(),
+      });
+    } else {
+      window.alert("Token contract not deployed to detected network");
+    }
+
+    // load PikaSwap
+    const pikaSwapData = PikaSwap.networks[networkId];
+    if (pikaSwapData) {
+      const pikaSwap = new web3.eth.Contract(
+        PikaSwap.abi,
+        pikaSwapData.address
+      );
+      this.setState({ pikaSwap });
+    } else {
+      window.alert("PikaSwap contract not deployed to detected network");
+    }
+    // console.log(this.state.pikaSwap);
+    this.setState({ loading: false });
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      account: "",
+      token: {},
+      pikaSwap: {},
+      ethBalance: "0",
+      tokenBalance: "0",
+      loading: true,
+    };
+  }
+
+
+
   render() {
+
+    let content;
+
+    if (this.state.loading) {
+      content = (
+        <p id='loader' className='text-center'>
+          Loading...
+        </p>
+      );
+    } else {
+      content = (
+        <p >
+          pikaexchange
+        </p>
+    }
     return (
       <div>
-        <p>pikachill</p>
+        <Navbar account={this.state.account} />
+        <div className='container-fluid mt-5'>
+          <div className='row'>
+            <main role='main' className='col-lg-12 d-flex text-center'>
+              <div className='content mr-auto ml-auto'>{content}</div>
+            </main>
+          </div>
+        </div>
       </div>
     );
   }
---
+++ src/components/Navbar.js
@@ -0,0 +1,50 @@
+import React, { useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+
+import pikachu from "../assets/icon1.png";
+import Jdenticon from "react-jdenticon";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+export default function ButtonAppBar({ account }) {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position='static' style={{ background: "#89aaff" }}>
+        <Toolbar>
+          <IconButton
+            edge='start'
+            className={classes.menuButton}
+            color='grey'
+            aria-label='menu'>
+            <img src={pikachu} width='48' />
+          </IconButton>
+          <Typography variant='h6' className={classes.title}>
+            Pikachu Coinbase
+          </Typography>
+          <Button color='inherit'>
+            <Jdenticon size='40' value={account} />
+          </Button>
+
+          <Typography>{account}</Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
